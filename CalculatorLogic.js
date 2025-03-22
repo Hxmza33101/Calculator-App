de3f5a673864e7c55187d85fcf8b58297188b8f6
Hxmza33101 <hkmughal2007@gmail.com>
2025-03-22 14:58:58 +1100

    Added some comments

--- CalculatorLogic.js
+++ CalculatorLogic.js
@@ -1,18 +1,18 @@
 const display = document.getElementById('display');
 
-function appendValue(value) {
+function appendValue(value) { //Adds the display value with the value
     display.value += value;
 }
 
-function clearDisplay() {
+function clearDisplay() { //Clears the display
     display.value = ' ';
 }
 
-function deleteDigit() {
+function deleteDigit() { //Removes the last digit
     display.value = display.value.slice(0, -1);
 }
 
-function calculateResult() {
+function calculateResult() { //Calculates this using a simple code
     try {
         display.value = eval(display.value);
     } catch {
